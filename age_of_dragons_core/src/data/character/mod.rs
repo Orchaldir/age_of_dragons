de0abcbb282038d40f3ebb5f1c2da658dbf356ec
Orchaldir <orchaldir@gmail.com>
2022-06-17 12:00:01 +0200

    [#1] Add Date

--- age_of_dragons_core/src/data/character/mod.rs
+++ age_of_dragons_core/src/data/character/mod.rs
@@ -1,6 +1,7 @@
 use crate::data::character::gender::Gender;
 use crate::data::character::race::{Race, RaceId};
 use crate::data::name::Name;
+use crate::data::time::{Date, Duration};
 use anyhow::{bail, Result};
 
 pub mod gender;
@@ -27,6 +28,8 @@ pub struct Character {
     name: Name,
     race_id: RaceId,
     gender: Gender,
+    birth_date: Date,
+    death_date: Option<Date>,
 }
 
 impl Character {
@@ -48,6 +51,8 @@ impl Character {
             name: Name::new(format!("Character {}", id.0)).unwrap(),
             race_id: RaceId::new(0),
             gender,
+            birth_date: Date::default(),
+            death_date: None,
         }
     }
 
@@ -55,21 +60,30 @@ impl Character {
     ///
     /// ```
     ///# use age_of_dragons_core::data::character::{Character, CharacterId};
-    ///# use age_of_dragons_core::data::character::gender::Gender;
+    ///# use age_of_dragons_core::data::character::gender::Gender::*;
     ///# use age_of_dragons_core::data::character::race::{Race, RaceId};
-    ///# use age_of_dragons_core::data::character::race::gender::GenderOption::*;
+    ///# use age_of_dragons_core::data::character::race::gender::GenderOption;
     ///# use age_of_dragons_core::data::character::race::stage::LifeStage;
     ///# use age_of_dragons_core::data::name::Name;
+    ///# use age_of_dragons_core::data::time::Date;
     /// let stage = LifeStage::simple();
-    /// let race = Race::simple(32, TwoGenders, vec![stage]);
+    /// let race = Race::simple(32, GenderOption::TwoGenders, vec![stage]);
     /// let id = CharacterId::new(11);
     /// let name = Name::new("C0").unwrap();
+    /// let date = Date::new(20);
     ///
-    /// assert!(Character::validate(id, name.clone(), &race, Gender::Female).is_ok());
-    /// assert!(Character::validate(id, name.clone(), &race, Gender::Male).is_ok());
-    /// assert!(Character::validate(id, name, &race, Gender::NoGender).is_err());
+    /// assert!(Character::validate(id, name.clone(), &race, Female, date, None).is_ok());
+    /// assert!(Character::validate(id, name.clone(), &race, Male, date, None).is_ok());
+    /// assert!(Character::validate(id, name, &race, NoGender, date, None).is_err());
     /// ```
-    pub fn validate(id: CharacterId, name: Name, race: &Race, gender: Gender) -> Result<Self> {
+    pub fn validate(
+        id: CharacterId,
+        name: Name,
+        race: &Race,
+        gender: Gender,
+        birth_date: Date,
+        death_date: Option<Date>,
+    ) -> Result<Self> {
         if !race.gender_option().is_valid(gender) {
             bail!(
                 "Character {} is invalid, because {:?} doesn't match the race's {:?}!",
@@ -84,6 +98,8 @@ impl Character {
             name,
             gender,
             race_id: race.id(),
+            birth_date,
+            death_date,
         })
     }
 
@@ -97,5 +113,30 @@ impl Character {
 
     pub fn gender(&self) -> Gender {
         self.gender
+    }
+
+    pub fn birth_date(&self) -> Date {
+        self.birth_date
+    }
+
+    pub fn death_date(&self) -> Option<Date> {
+        self.death_date
+    }
+
+    pub fn is_alive(&self) -> bool {
+        self.death_date.is_none()
+    }
+
+    pub fn is_dead(&self) -> bool {
+        self.death_date.is_some()
+    }
+
+    pub fn calculate_age(&self, date: Date) -> Duration {
+        if let Some(death_date) = self.death_date {
+            death_date
+        } else {
+            date
+        }
+        .get_duration_since(self.birth_date)
     }
 }
