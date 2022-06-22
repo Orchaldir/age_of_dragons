fe5548afe867abdedc527523fa71607b4a581c96
Orchaldir <orchaldir@gmail.com>
2022-06-22 20:42:37 +0200

    [#4] Refactor calculate_aging()

--- age_of_dragons_core/src/simulation/character/aging.rs
+++ age_of_dragons_core/src/simulation/character/aging.rs
@@ -1,4 +1,4 @@
-use crate::data::character::CharacterId;
+use crate::data::character::{Character, CharacterId};
 use crate::data::SimulationData;
 
 enum AgingEffect {
@@ -14,37 +14,40 @@ fn calculate_aging_effects(data: &SimulationData) -> Vec<AgingEffect> {
     data.character_manager
         .get_all()
         .iter()
-        .filter_map(|character| {
-            if character.is_dead() {
-                return None;
-            }
-
-            let race = data
-                .race_manager
-                .get(character.race_id())
-                .expect("Character's race is unknown!");
-
-            let stage = race
-                .stages()
-                .get(character.life_stage())
-                .expect("Character's life stage is unknown!");
-
-            if let Some(max_age) = stage.max_age() {
-                let age = character.calculate_age(data.date);
-
-                if age > *max_age {
-                    let new_life_stage = character.life_stage() + 1;
-                    let is_last_stage = new_life_stage == race.stages().len();
-
-                    if is_last_stage {
-                        return Some(AgingEffect::Death(character.id()));
-                    } else {
-                        return Some(AgingEffect::ChangeLifeStage(character.id(), new_life_stage));
-                    }
-                }
-            }
-
-            None
-        })
+        .filter_map(|character| calculate_aging_effect(data, character))
         .collect()
+}
+
+/// Calculates if a [`Character`](crate::data::character::Character) is effected by aging.
+fn calculate_aging_effect(data: &SimulationData, character: &Character) -> Option<AgingEffect> {
+    if character.is_dead() {
+        return None;
+    }
+
+    let race = data
+        .race_manager
+        .get(character.race_id())
+        .expect("Character's race is unknown!");
+
+    let stage = race
+        .stages()
+        .get(character.life_stage())
+        .expect("Character's life stage is unknown!");
+
+    if let Some(max_age) = stage.max_age() {
+        let age = character.calculate_age(data.date);
+
+        if age > *max_age {
+            let new_life_stage = character.life_stage() + 1;
+            let is_last_stage = new_life_stage == race.stages().len();
+
+            return if is_last_stage {
+                Some(AgingEffect::Death(character.id()))
+            } else {
+                Some(AgingEffect::ChangeLifeStage(character.id(), new_life_stage))
+            };
+        }
+    }
+
+    None
 }
