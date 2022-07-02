a5bad4993009148ddbb54af738a03d1c66d11b4f
Orchaldir <orchaldir@gmail.com>
2022-07-02 08:31:57 +0200

    [#2] Add CharacterRelation

---
+++ age_of_dragons_core/src/data/character/relation/mod.rs
@@ -0,0 +1,14 @@
+use crate::data::character::CharacterId;
+
+/// The type of a relationship between 2 [`Characters`](crate::data::character::Character).
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum CharacterRelationType {
+    Mate,
+}
+
+/// A relationship between 2 [`Characters`](crate::data::character::Character).
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct CharacterRelation {
+    id: CharacterId,
+    relation_type: CharacterRelationType,
+}
