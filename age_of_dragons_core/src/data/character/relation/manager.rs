9ac387b1bc52c6fb6040589c3554f66cae7a695c
Orchaldir <orchaldir@gmail.com>
2022-07-02 09:56:12 +0200

    [#2] Improve CharacterRelationMgr

--- age_of_dragons_core/src/data/character/relation/manager.rs
+++ age_of_dragons_core/src/data/character/relation/manager.rs
@@ -1,4 +1,4 @@
-use crate::data::character::relation::CharacterRelation;
+use crate::data::character::relation::{CharacterRelation, CharacterRelationType};
 use crate::data::character::CharacterId;
 
 /// Stores the [`relations`](CharacterRelation) between all [`Characters`](crate::data::character::Character).
@@ -9,6 +9,32 @@ pub struct CharacterRelationMgr {
 }
 
 impl CharacterRelationMgr {
+    /// Returns all the [`relations`](CharacterRelation) between 2 [`Characters`](crate::data::character::Character).
+    pub fn add_relation_between(
+        &mut self,
+        id0: CharacterId,
+        id1: CharacterId,
+        relation_type: CharacterRelationType,
+    ) {
+        self.check_size(id0, id1);
+        self.add_relation(id0, id1, relation_type);
+        self.add_relation(id1, id0, relation_type);
+    }
+
+    fn add_relation(
+        &mut self,
+        id0: CharacterId,
+        id1: CharacterId,
+        relation_type: CharacterRelationType,
+    ) {
+        if let Some(relations) = self.relations.get_mut(id0.id()) {
+            relations.push(CharacterRelation {
+                id: id1,
+                relation_type,
+            })
+        }
+    }
+
     /// Returns all the [`relations`](CharacterRelation) between 2 [`Characters`](crate::data::character::Character).
     pub fn get_relations_between(
         &self,
@@ -29,5 +55,18 @@ impl CharacterRelationMgr {
     /// Returns all the [`relations`](CharacterRelation) of a [`Character`](crate::data::character::Character).
     pub fn get_relations_of(&self, id: CharacterId) -> &[CharacterRelation] {
         self.relations.get(id.id()).unwrap_or(&self.no_relations)
+    }
+
+    /// Makes sure that the vector of relations is large enough to contain both characters.
+    fn check_size(&mut self, id0: CharacterId, id1: CharacterId) {
+        let min_size = id0.0.max(id1.0);
+
+        if self.relations.len() <= min_size {
+            let missing_characters = (min_size - self.relations.len()) + 1;
+
+            for _i in 0..missing_characters {
+                self.relations.push(Vec::new());
+            }
+        }
     }
 }
